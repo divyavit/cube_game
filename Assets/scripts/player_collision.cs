46bbb081ac30f839337023218b92c982a01214a0
FirzenYogesh <contact@syogesh.in>
2019-12-05 19:12:49 +0530

    first commit

---
+++ Assets/scripts/player_collision.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class player_collision : MonoBehaviour {
+    // Start is called before the first frame update
+    public player_movement movement;
+    // Update is called once per frame
+
+    private void OnCollisionEnter(Collision collisioninfo) {
+
+        if(collisioninfo.collider.tag == "obstacle") {
+            movement.enabled = false;
+            FindObjectOfType<game_manager>().endgame();
+        }
+    }
+}
---
+++ Assets/scripts/player_movement.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardforce = 500f;
+    public float sidewaysforce = 500f;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardforce * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(sidewaysforce * Time.deltaTime,0,0,ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-sidewaysforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if(rb.position.y< -1f) {
+            FindObjectOfType<game_manager>().endgame();
+        }
+    }
+}
