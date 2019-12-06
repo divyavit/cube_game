5b13406367f1e7c809315e6f680a07c6890740b8
FirzenYogesh <contact@syogesh.in>
2019-12-06 19:04:52 +0530

    done

--- Assets/scripts/ending.cs
+++ Assets/scripts/ending.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class ending : MonoBehaviour
 {
     public game_manager gamemanager;
+
+
     // Start is called before the first frame update
     void OnTriggerEnter() {
-        gamemanager.completelevel();
+         { gamemanager.completelevel(); }
     }
 }
 
--- Assets/scripts/player_movement.cs
+++ Assets/scripts/player_movement.cs
@@ -7,7 +7,7 @@ public class player_movement : MonoBehaviour
     public float forwardforce = 500f;
     public float sidewaysforce = 500f;
     // Start is called before the first frame update
-
+    public bool onground=true;
 
     // Update is called once per frame
     void FixedUpdate()
@@ -21,8 +21,20 @@ public class player_movement : MonoBehaviour
         {
             rb.AddForce(-sidewaysforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
+        if(onground) {
+            if(Input.GetKey(KeyCode.W)) {
+                rb.AddForce(new Vector3(0, 7, 0), ForceMode.Impulse);
+                onground = false;
+            }
+        }
+
         if(rb.position.y< -1f) {
             FindObjectOfType<game_manager>().endgame();
+        }
+    }
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.CompareTag("ground")) {
+            onground = true;
         }
     }
 }
